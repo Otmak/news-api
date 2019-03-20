91ea7173324ea32204cdf22ab13a9b8648daf284
Otma <jakelovz@gmail.com>
2019-03-19 23:46:45 -0700

    -2 steps back same code but with map(); and better hover effects

--- app/index.js
+++ app/index.js
@@ -1,5 +1,3 @@
-
-
 const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'main') 
@@ -14,73 +12,60 @@ fetch(url)
           //let print = data.result
           let eachArticle = data.articles;
           //let apiDesc = data.articles;
-        
+          eachArticle.map(function(eachArticle) {
 
-          eachArticle.map( function(eachArticle){
-          
-          
-                    let card = document.createElement('div');
-                    card.setAttribute('class', 'card');
-                    card.setAttribute('data-toggle-id', 'readMore')
-          
-          
-                    eachArticle.urlToImage;
-                    eachArticle.title;
-                    eachArticle.description;
-          
-                    //data.articles.description
-                    let aLink = eachArticle.url
-          
-                    //IMAGE AND ANCHOR
-                    const img = document.createElement('img');
-                        img.setAttribute('class', 'newapi-img');
-                        const a = document.createElement('a');
-                        a.setAttribute('href', aLink);
-                        a.setAttribute('target', '_blank');
-          
-          
-                    //TITLE AND DESCRIPTION && img source
-                    const h1 = document.createElement('h1');
-                    const p = document.createElement('p');
-                    const infoP = document.createElement('p');
-                    //infoP.setAttribute('hidden');
-                    const reedmo = document.getElementById('reedMo');
-          
-                    // if image source in empty get my img
-                    if(eachArticle.urlToImage == null){
-                        img.src = './image-not-available.jpg'
-                    }else{
-                        img.src = eachArticle.urlToImage;
-                    }
-                    
-                    h1.textContent = eachArticle.title;
-                    p.textContent = eachArticle.description;
-                 
-                         // PASTE CARDS
-                          container.appendChild(card)
-                          card.appendChild(img)
-                          card.appendChild(h1)
-                          card.appendChild(p)
-                          card.appendChild(infoP)
-                          card.appendChild(reedmo)
-                    
-          
-                    // Event deli to read info on click 'toggle type'
-                    
-              
-                    //console LOG
-                    console.log(data.articles)
-                    console.log(data.articles.description)
-                    console.log(eachArticle.description)
-          
-          
-                     //data.articles.description
-          
-          
+          const card = document.createElement('div');
+          card.setAttribute('class', 'card');
 
-    });
 
-            
+          eachArticle.urlToImage;
+          eachArticle.title;
+          eachArticle.description;
 
-          //console LOG          
+          //data.articles.description
+          let aLink = eachArticle.url
+
+          //IMAGE AND ANCHOR TAG
+          const img = document.createElement('img');
+              img.setAttribute('class', 'newapi-img');
+              const a = document.createElement('a');
+              a.setAttribute('href', aLink);
+              //open a in new tab
+              a.setAttribute('target', '_blank');
+              a.setAttribute('rel', 'noopener noreferrer');
+
+
+          //TITLE AND DESCRIPTION
+          const h1 = document.createElement('h1');
+          const p = document.createElement('p');
+              if(eachArticle.urlToImage == null){
+
+                  img.src = './images/image-not-available.jpg'
+
+              }else{
+
+                img.src = img.src = eachArticle.urlToImage;
+
+              }
+
+                h1.textContent = eachArticle.title;
+                p.textContent = eachArticle.description;
+                // PASTE CARDS
+                /*container.appendChild(card)
+                card.appendChild(img)
+                card.appendChild(h1)
+                card.appendChild(p)*/
+                container.appendChild(a)
+                a.appendChild(card)
+                card.appendChild(img)
+                card.appendChild(h1)
+                card.appendChild(p)
+
+
+
+                console.log(data.articles)
+                console.log(data.articles.description)
+
+               });
+
      });
