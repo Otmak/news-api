ffe164f80d6fd04c04b5c2ac15ec947c20bf2755
Otma <jakelovz@gmail.com>
2019-03-30 20:30:23 -0700

    :)

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
+/*  
+  Just had to prove to myself that calling an API and pasting it on a page Is pretty F++king simple 
+*/
+
+
 const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'main') 
@@ -9,9 +14,9 @@ let url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=ee4ec461eceb4e
 fetch(url)
      .then((resp) => resp.json())
      .then((data) =>{
-          //let print = data.result
+          
           let eachArticle = data.articles;
-          //let apiDesc = data.articles;
+          
           eachArticle.map(function(eachArticle) {
 
           const card = document.createElement('div');
@@ -21,8 +26,6 @@ fetch(url)
           eachArticle.urlToImage;
           eachArticle.title;
           eachArticle.description;
-
-          //data.articles.description
           let aLink = eachArticle.url
 
           //IMAGE AND ANCHOR TAG
@@ -30,41 +33,30 @@ fetch(url)
               img.setAttribute('class', 'newapi-img');
               const a = document.createElement('a');
               a.setAttribute('href', aLink);
+
               //open a in new tab
               a.setAttribute('target', '_blank');
               a.setAttribute('rel', 'noopener noreferrer');
 
 
-          //TITLE AND DESCRIPTION
-          const h1 = document.createElement('h1');
-          const p = document.createElement('p');
-              if(eachArticle.urlToImage == null){
-
+              //TITLE AND DESCRIPTION
+              const h1 = document.createElement('h1');
+              const p = document.createElement('p');
+              if(eachArticle.urlToImage == null)
+              {
                   img.src = './images/image-not-available.jpg'
-
               }else{
-
-                img.src = img.src = eachArticle.urlToImage;
-
-              }
+                  img.src = img.src = eachArticle.urlToImage;
+                   }
 
                 h1.textContent = eachArticle.title;
                 p.textContent = eachArticle.description;
-                // PASTE CARDS
-                /*container.appendChild(card)
-                card.appendChild(img)
-                card.appendChild(h1)
-                card.appendChild(p)*/
+                
                 container.appendChild(a)
                 a.appendChild(card)
                 card.appendChild(img)
                 card.appendChild(h1)
                 card.appendChild(p)
-
-
-
-                console.log(data.articles)
-                console.log(data.articles.description)
 
                });
 
